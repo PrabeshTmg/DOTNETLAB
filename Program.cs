0439cf8ed95b5f55a05f366861e9fce724a6c3d1
Prabesh Tamang <tamangprabesh360@gmail.com>
2023-01-04 09:50:58 +0545

    Attempt to access the variable outside of the code block in which it was defined

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿bool flag = true;
-int value = 0;
-
 if (flag)
 {
-    value = 10;
+    int value = 10;
     Console.WriteLine("Inside of code block: " + value);
 }
-Console.WriteLine("Outside of code block: " + value);
+Console.WriteLine($"Outside of code block: {value}");
