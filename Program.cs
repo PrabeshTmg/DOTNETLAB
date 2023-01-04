64d7739f86f4b0ca4737a2ffcd6b6c7dd3232a1d
Prabesh Tamang <tamangprabesh360@gmail.com>
2023-01-04 18:28:34 +0545

    Write code to find an opening and closing parenthesis embedded in a string

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+﻿string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
